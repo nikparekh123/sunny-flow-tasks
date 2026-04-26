8bb768aa311d6871d43e972504efed536d49dc2d
Niket Parekh <nik@sunnyfi.co>
2026-04-26 19:57:24 -0400

    Add Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { Toaster } from '@/components/ui/toaster';
 import { TooltipProvider } from '@/components/ui/tooltip';
@@ -89,6 +90,7 @@ const App = () => {
           <Toaster />
           <Sonner />
           <BrowserRouter>{content}</BrowserRouter>
+          <SpeedInsights />
         </TooltipProvider>
       </AuthProvider>
     </QueryClientProvider>
